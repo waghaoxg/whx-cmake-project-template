f9330601523a0ff2fcca457ff0535b5f8ed1dea6
Haoxing Wang <haoxingw@gmail.com>
2014-08-17 15:26:13 +0800

    init

---
+++ src/runShowPic/runShowPic.cpp
@@ -0,0 +1,17 @@
+#include "highgui/highgui.hpp"
+#include "core/core.hpp"
+#include "config.h"
+#include <iostream>
+#include "core/core.hpp"
+
+using namespace cv;
+using namespace std;
+
+int main()
+{
+	//Mat img = imread("E:\\pic\\mymy\\DSC_0001_800h.png");
+	//imshow("kankakn",img);
+#ifdef PRINT_INFO
+	cout << "show print info" << endl;
+#endif
+}
